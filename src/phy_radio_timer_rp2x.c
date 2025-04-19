afbc222f56ac2cb95ce99dcbed8fd31f4814e68c
Lucas Wennerholm <lucas.wennerholm@gmail.com>
2025-04-19 21:36:05 +0200

    Use PWM timer for sync

--- src/phy_radio_timer_rp2x.c
+++ src/phy_radio_timer_rp2x.c
@@ -21,15 +21,22 @@
  */
 
 #include "phy_radio_timer.h"
+#include "phy_radio.h"
 #include "pico/stdlib.h"
 #include "pico/time.h"
+#include "hardware/pwm.h"
+#include "hardware/irq.h"
+#include "hardware/clocks.h"
+#include "math.h"
 
 struct phyRadioTimerInternal {
-    repeating_timer_t repeating_timer;
-    bool              repeating_timer_active;
-    alarm_id_t        task_alarm_id;    // Timer alarm used for time dependent tasks
-    alarm_id_t        sync_alarm_id;    // Timer alarm used for synchronization with Central
-    alarm_id_t        prepare_alarm_id; // Timer alarm used to start the guard period
+    bool       repeating_timer_active;
+    float      clk_div;
+    float      clk_converter;
+    alarm_id_t task_alarm_id;    // Timer alarm used for time dependent tasks
+    alarm_id_t sync_alarm_id;    // Timer alarm used for synchronization with Central
+    alarm_id_t prepare_alarm_id; // Timer alarm used to start the guard period
+    phyRadioTimer_t *inst;
 };
 
 static phyRadioTimerInternal_t timer_data;
@@ -64,13 +71,12 @@ static int64_t prepare_timer_alarm_callback(alarm_id_t id, void *user_data) {
     return PHY_RADIO_TIMER_SUCCESS;
 }
 
-static bool repeating_timer_callback(__unused struct repeating_timer *t) {
+static void repeating_timer_callback(void) {
     // Get the current phy radio instance
-    phyRadioTimer_t *inst =  (phyRadioTimer_t*)t->user_data;
+    phyRadioTimer_t *inst =  timer_data.inst;
 
+    pwm_clear_irq(0);
     inst->repeating_cb(inst);
-
-    return true;
 }
 
 /**
@@ -94,6 +100,23 @@ int32_t phyRadioTimerInit(phyRadioTimer_t *inst) {
     inst->task_cb      = NULL;
     inst->repeating_cb = NULL;
     inst->_private     = &timer_data;
+    timer_data.inst    = inst;
+
+    float sys_hz = (float)clock_get_hz(clk_sys);
+    timer_data.clk_div = ceilf((PHY_RADIO_SLOT_TIME_US/1000000.0f)/((65000.0f/sys_hz)));
+    timer_data.clk_converter = 0.000001f/(1.0f/(sys_hz/timer_data.clk_div));
+
+    // Configure PWM slice 0 for a period of period_us
+    uint slice = 0;
+    pwm_config config = pwm_get_default_config();
+    pwm_config_set_clkdiv(&config, timer_data.clk_div);  // 1 tick = 1 us
+    pwm_init(slice, &config, false);
+
+    uint32_t ticks = (uint32_t)PHY_RADIO_SLOT_TIME_US * timer_data.clk_converter;
+    pwm_config_set_wrap(&config, ticks);
+    pwm_clear_irq(slice);
+    irq_set_exclusive_handler(PWM_DEFAULT_IRQ_NUM(), repeating_timer_callback);
+    irq_set_enabled(PWM_DEFAULT_IRQ_NUM(), true);
 
     timer_data.task_alarm_id          = 0;    // Timer alarm used for time dependent tasks
     timer_data.prepare_alarm_id       = 0; // Timer alarm used to start the guard period
@@ -127,12 +150,7 @@ int32_t phyRadioTimerCancelAll(phyRadioTimer_t *inst) {
     inst->repeating_cb = NULL;
 
     // Cancel active timer
-    if (inst->_private->repeating_timer_active) {
-        if (!cancel_repeating_timer(&inst->_private->repeating_timer)) {
-            return PHY_RADIO_TIMER_HAL_ERROR;
-        }
-        inst->_private->repeating_timer_active = false;
-    }
+    phyRadioTimerStopRepeatingTimer(inst);
 
     // Cancel any active alarm
     if (inst->_private->prepare_alarm_id != 0) {
@@ -241,29 +259,50 @@ int32_t phyRadioTimerCancelPrepareTimer(phyRadioTimer_t *inst) {
 }
 
 int32_t phyRadioTimerStartRepeatingTimer(phyRadioTimer_t *inst, phyRadioTimerCb_t cb, uint32_t period_us) {
-    if (!add_repeating_timer_us(period_us, repeating_timer_callback, inst, &inst->_private->repeating_timer)) {
-        return PHY_RADIO_TIMER_HAL_ERROR;
+    if (timer_data.repeating_timer_active) {
+        return PHY_RADIO_TIMER_ACTIVE_ERROR;
     }
 
     inst->repeating_cb = cb;
-    inst->_private->repeating_timer_active = true;
+    uint slice = 0;
+    uint32_t ticks = (uint32_t)period_us * timer_data.clk_converter;
+ 
+    // Start PWM counter
+    pwm_set_wrap(slice, ticks);
+    pwm_set_counter(slice, 0);
+
+    // Enable IRQ on wrap
+    pwm_set_irq_enabled(slice, true);
+    pwm_set_enabled(slice, true);
+
+    // Save state
+    timer_data.repeating_timer_active = true;
 
     return PHY_RADIO_TIMER_SUCCESS;
 }
 
 int32_t phyRadioTimerStopRepeatingTimer(phyRadioTimer_t *inst) {
-    if (inst->_private->repeating_timer_active) {
-        if (!cancel_repeating_timer(&inst->_private->repeating_timer)) {
-            return PHY_RADIO_TIMER_HAL_ERROR;
-        }
+    if (timer_data.repeating_timer_active) {
+        uint slice = 0;
+        pwm_set_irq_enabled(slice, false);
+        pwm_set_enabled(slice, false);
+        timer_data.repeating_timer_active = false;
     }
 
-    inst->_private->repeating_timer_active = false;
     inst->repeating_cb = NULL;
-
     return PHY_RADIO_TIMER_SUCCESS;
 }
 
+/**
+ * Update the PWM period on the fly
+ */
 int32_t phyRadioTimerUpdateRepeatingTimer(phyRadioTimer_t *inst, uint32_t new_period_us) {
+    if (!timer_data.repeating_timer_active) {
+        return PHY_RADIO_TIMER_ACTIVE_ERROR;
+    }
+
+    uint32_t ticks = (uint32_t)new_period_us * timer_data.clk_converter;
+    uint slice = 0;
+    pwm_set_wrap(slice, ticks);
     return PHY_RADIO_TIMER_SUCCESS;
 }
